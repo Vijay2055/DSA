524274c808600f556b70973730e578b7437d7020
Bijay Yadav <113086572+Vijay2055@users.noreply.github.com>
2022-12-20 00:09:08 +0530

    Update reverseNegativeArray.cpp

--- Array/reverseNegativeArray.cpp
+++ Array/reverseNegativeArray.cpp
@@ -35,3 +35,18 @@ int main() {
     }cout<<endl;
 	return 0;
 }
+
+//second approach
+
+void sortArray(int arr[],int n){
+   int j=0;
+   for(int i=0;i<n;i++){
+       if(arr[i]<0){
+           if(i!=j){
+               swap(arr[i],arr[j]);
+           }
+           j++;
+       }
+   }
+    
+}
