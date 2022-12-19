a6dfcd5bdcfedc784b1c6e49f9c06977189395a0
Bijay Yadav <113086572+Vijay2055@users.noreply.github.com>
2022-12-19 23:37:02 +0530

    Create reverseNegativeArray.cpp

---
+++ Array/reverseNegativeArray.cpp
@@ -0,0 +1,37 @@
+//Reverse an array where -ve elements are at beggining side and +ve elements are at right side
+//don't care about the order of the element 
+
+#include <iostream>
+using namespace std;
+
+void reverseArray(int arr[],int n){
+    int i=0;
+    int j=n-1;
+    while(i<=j){
+        if(arr[i]<0){
+            i++;
+        }else if(arr[j]>=0){
+            j--;
+        }else{
+            swap(arr[i],arr[j]);
+            i++;j--;
+        }
+    }
+    
+}
+
+int main() {
+     int arr[100];
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    
+   
+    reverseArray(arr,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }cout<<endl;
+	return 0;
+}
