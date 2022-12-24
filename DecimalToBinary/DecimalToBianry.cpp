2572ae5ae1e7686f50937ec493d9c6eb4b8c5f42
Bijay Yadav <113086572+Vijay2055@users.noreply.github.com>
2022-12-25 01:16:34 +0530

    Update DecimalToBianry.cpp

--- DecimalToBinary/DecimalToBianry.cpp
+++ DecimalToBinary/DecimalToBianry.cpp
@@ -24,3 +24,25 @@ int main()
     int x = 1234;
     findBinary(x);
 }
+
+//Another approach
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    int ans=0;
+    int i=0;
+    while(n!=0){
+        int bit=n&1;
+         ans=(ans+bit*pow(10,i));
+         cout<<ans<<endl;
+        n=n>>1;
+        i++;
+    }
+    
+}
+
+
+//Another approach
