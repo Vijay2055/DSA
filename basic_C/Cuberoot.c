5df7bab10c13214843f880ed6b71162f3c9d96c1
Bijay Yadav <113086572+Vijay2055@users.noreply.github.com>
2023-03-01 23:42:20 +0530

    Create Cuberoot.c

---
+++ basic_C/Cuberoot.c
@@ -0,0 +1,10 @@
+//program for finding cuberoot
+#include"stdio.h"
+#include"math.h"
+int main() 
+{ 
+        int n;
+        printf("Enter the number : ");
+        scanf("%d",&n);
+        printf("value %f",cbrt(n));
+}
