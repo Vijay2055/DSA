74a56a096183c077b442ee99d3f3e6bee35a4b03
Bijay Yadav <113086572+Vijay2055@users.noreply.github.com>
2023-03-01 23:37:58 +0530

    Create 05_sqrt.c

---
+++ basic_C/05_sqrt.c
@@ -0,0 +1,11 @@
+//finding sqrt of a given num
+#include"stdio.h"
+#include"math.h"
+int main()
+{
+        int n;
+        printf("Enter number\n");
+        scanf("%d",&n);
+        printf("Value %f",sqrt(n));
+        return 0;
+}
